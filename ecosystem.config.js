c746e07fa6199de946dcec47588bb71c5c4f9546
bastianhilton <bastianh86@outlook.com>
2021-10-25 21:12:23 -0700

    updated cms

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,18 +1,18 @@
 module.exports = {
   "apps": [
     {
-      "name": "alternatecms",
-      "cwd": "../alternate-cms",
-      "args": ["runserver", "0.0.0.0:8000"], 
+      "name": "meeovilocate",
+      "cwd": "../alternatelocate",
+      "args": ["runserver", "0.0.0.0:8087"], 
       "script": "manage.py",
       "exec_mode": "fork",
       "exec_interpreter": "python"
     },
-    {
+    /*{
       "name": "graphqlmesh",
       "cwd": "../alternate-cms",
-      "args": ["yarn", "mesh", "0.0.0.0:4000"],
+      "args": ["yarn", "mesh", "0.0.0.0:4003"],
       "script": "dev",
-    }
+    }*/
   ]
 }
